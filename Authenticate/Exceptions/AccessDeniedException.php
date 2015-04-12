9f34e32d1063c63a8808ce4ff716522a22d4ce6a
Payam Naderi <naderi.payam@gmail.com>
2015-04-12 20:13:22 +0430

    implement error

--- Authenticate/Exceptions/AccessDeniedException.php
+++ Authenticate/Exceptions/AccessDeniedException.php
@@ -3,6 +3,15 @@ namespace Poirot\AuthSystem\Authenticate\Exceptions;
 
 class AccessDeniedException extends \RuntimeException
 {
+    const EXCEPTION_ACCESS_DENIED_DEF_MESSAGE = 'Access Denied';
 
+    function __construct(
+        $message = self::EXCEPTION_ACCESS_DENIED_DEF_MESSAGE,
+        $code = 403,
+        \Exception $previous = null
+    )
+    {
+        parent::__construct($message, $code, $previous);
+    }
 }
  
