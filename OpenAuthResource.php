52cf0a1fe09adfbccea37cbf44f44ba3a5d1c52f
Payam Naderi <naderi.payam@gmail.com>
2015-04-12 14:15:11 +0430

    code cleanup

--- OpenAuthResource.php
+++ OpenAuthResource.php
@@ -5,8 +5,7 @@ use Poirot\AuthSystem\Authorize\Interfaces\iAuthResource;
 use Poirot\Core\OpenOptions;
 
 class OpenAuthResource extends OpenOptions
-    implements
-    iAuthResource
+    implements iAuthResource
 {
 
 }
