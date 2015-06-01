f730ef1f26355a3d2ec6f93dd2fa6af02a4db781
Payam Naderi <naderi.payam@gmail.com>
2015-06-02 01:15:15 +0430

    code cleanup

--- AbstractCredential.php
+++ AbstractCredential.php
@@ -8,36 +8,6 @@ abstract class AbstractCredential extends AbstractOptions
     implements
     iCredential
 {
-    /**
-     * Used For Remember Me! Feature
-     * @var bool
-     */
-    protected $remember = false;
 
-    /**
-     * Remember Me Feature!
-     *
-     * @param bool $flag
-     *
-     * @return $this
-     */
-    function setRemember($flag = true)
-    {
-        $this->remember = $flag;
-
-        return $this;
-    }
-
-    /**
-     * If Authorization was successful identity
-     * will use this to fill user data from
-     * adapter to identity
-     *
-     * ! in database it can be a unique field like
-     *   mailAddress, pk, ...
-     *
-     * @return mixed
-     */
-    abstract function getUserIdentity();
 }
  
---
+++ Authenticate/Adapter/UserPassCredential.php
@@ -0,0 +1,49 @@
+<?php
+namespace Poirot\AuthSystem\Authenticate\Adapter;
+
+use Poirot\AuthSystem\AbstractCredential;
+
+class UserPassCredential extends AbstractCredential
+{
+    protected $username;
+    protected $password;
+
+    /**
+     * @return mixed
+     */
+    public function getUsername()
+    {
+        return $this->username;
+    }
+
+    /**
+     * @param mixed $username
+     * @return $this
+     */
+    public function setUsername($username)
+    {
+        $this->username = $username;
+
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getPassword()
+    {
+        return $this->password;
+    }
+
+    /**
+     * @param mixed $password
+     * @return $this
+     */
+    public function setPassword($password)
+    {
+        $this->password = $password;
+
+        return $this;
+    }
+}
+ 
