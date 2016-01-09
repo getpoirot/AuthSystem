790e3f9c0fa3a0ae69f2c665d10f3a5d957d689b
Payam Naderi <naderi.payam@gmail.com>
2016-01-09 11:43:44 +0330

    code cleanup

--- Poirot/AuthSystem/Authenticate/AbstractHttpAuthenticator.php
+++ Poirot/AuthSystem/Authenticate/AbstractHttpAuthenticator.php
@@ -11,6 +11,38 @@ use Poirot\Core\AbstractOptions;
 use Poirot\Http\Interfaces\Message\iHttpRequest;
 use Poirot\Http\Message\HttpRequest;
 
+/*
+$auth     = new Authenticator\HttpSessionAuth([
+    'identity' => $lazyLoad,
+    'request'  => $request,
+    'response' => $response,
+]);
+
+try {
+    $auth->authenticate();
+    if ($auth->isSignInRequestReceived())
+        $auth->signIn();
+
+    $response->setBody("<h1>Hello User {$auth->identity()->getEmail()}</h1>");
+} catch (WrongCredentialException $e) {
+    throw new \Exception('Invalid Username or Password.');
+} catch (UserNotFoundException $e) {
+    throw new \Exception('Invalid Username or Password.');
+} catch (AuthenticationException $e)
+{
+    $response->setBody('
+            <form method="post" action="" enctype="application/x-www-form-urlencoded">
+                 <input type="text" name="email">
+                 <input type="password" name="password">
+
+                 <input type="submit" value="send">
+            </form>
+        ');
+}
+
+$response->flush();
+*/
+
 abstract class AbstractHttpAuthenticator extends AbstractAuthenticator
     implements iAuthenticator
 {
