de083a363dc968329a6d909bf23c8cf2e9a01d51
Payam Naderi <naderi.payam@gmail.com>
2016-01-11 15:42:58 +0330

    rename

--- Poirot/AuthSystem/Authenticate/Exceptions/PermissionDeniedException.php
+++ Poirot/AuthSystem/Authenticate/Exceptions/PermissionDeniedException.php
@@ -1,7 +1,7 @@
 <?php
 namespace Poirot\AuthSystem\Authenticate\Exceptions;
 
-class AccessDeniedException extends AuthenticationException
+class PermissionDeniedException extends AuthenticationException
 {
     const EXCEPTION_DEFAULT_MESSAGE = 'Access Denied';
 }
